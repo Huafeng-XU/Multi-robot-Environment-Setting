5d7eb49954c3fbcfa92dc5f33aa058ae368801c1
Huafeng-XU <56384517+Huafeng-XU@users.noreply.github.com>
2022-06-21 15:36:35 +0800

    Update startup.py

--- startup.py
+++ startup.py
@@ -1,6 +1,11 @@
-
-# start the bringup.sh
+## in pi
+# start the bringup.sh 
 ./bringup.sh
+
+# start the low-level control
+srobot...
+
+### in remote pc
 # start extrinsinc camera
 export AUTO_EX_CALIB=calibration
 roslaunch turtlebot3_autorace_camera turtlebot3_autorace_extrinsic_camera_calibration.launch
@@ -11,3 +16,16 @@ roslaunch turtlebot3_autorace_detect turtlebot3_autorace_detect_lane.launch
 rqt
 # start configuration
 rosrun rqt_reconfigure rqt_reconfigure
+
+
+
+# for pipe robot
+## check the linux environment for runing
+conda env list
+## activate the pipe-ins
+conda activate pipe-ins
+## start recieved
+python3 app2.py
+
+## in pi
+python3 push
